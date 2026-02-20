8cc7dcb0da25412b38e00f28ded074599911a61f
arcalise08 <arcalise08@gmail.com>
2026-02-20 10:16:39 -0600

    updated to even simplier example

--- FusionBug.Downstream/Graph/FooGraph.cs
+++ FusionBug.Downstream/Graph/FooGraph.cs
@@ -5,21 +5,11 @@ namespace FusionBug.Downstream.Graph;
 
 public class Utilities
 {
-    public static IError ConvertResultError(string message, List<ValidationFailure> failures)
+    public static IError ConvertResultError(string message, List<string> failures)
     {
         return ErrorBuilder.New()
             .SetMessage(message ?? "An error occurred")
-            .SetExtension("validationFailures",
-                failures
-                    .Select(x => new Dictionary<string, object?>
-                    {
-                        { "errorCode", x.ErrorCode },
-                        { "errorMessage", x.ErrorMessage },
-                        { "propertyName", x.PropertyName },
-                        { "attemptedValue", x.AttemptedValue },
-                        { "severity", x.Severity }
-                    })
-                    .ToList())
+            .SetExtension("validationFailures", failures)
             .Build();
     }
 
@@ -30,7 +20,7 @@ public class FooGraph
     public async Task<IEnumerable<Foo>> GetFoo(string? barInput)
     {
         throw new GraphQLException(
-            Utilities.ConvertResultError("validation failure", new List<ValidationFailure>())
+            Utilities.ConvertResultError("validation failure", [])
         );
     }
 }
